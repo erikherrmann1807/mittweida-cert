4eb51d0f0d086a04106aacc2ae2422a086356548
Erik Herrmann <erikherrmann1@gmx.de>
2026-01-12 11:00:17 +0100

    Add missing landing page content

--- content/landing_page/privacy_policy.py
+++ content/landing_page/privacy_policy.py
@@ -1,5 +1,10 @@
 import streamlit as st
 
+from util import get_config
+
+config = get_config()
 
 def privacy_policy_section():
-    st.markdown("Datenschutz")
+    data_privacy_cfg = config['texts'][st.session_state.language]['landing_page']['data_privacy']
+    with st.expander(f"{data_privacy_cfg['header']}"):
+        st.markdown(f"{data_privacy_cfg['privacy_policy']}", unsafe_allow_html=True)
