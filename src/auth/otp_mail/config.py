b01cd35665c8acca8e8ba6347e489b554be50190
Erik Herrmann <erikherrmann1@gmx.de>
2026-03-04 13:50:12 +0100

    Add missing localization

--- src/auth/otp_mail/config.py
+++ src/auth/otp_mail/config.py
@@ -12,14 +12,3 @@ SMTP_USER = SMTP_CFG.get("user", "")
 SMTP_PASS = SMTP_CFG.get("password", "")
 SMTP_FROM = SMTP_CFG.get("from", SMTP_USER)
 USE_STARTTLS = bool(SMTP_CFG.get("use_starttls", True))
-
-# ---------- OTP-Parameter ----------
-CODE_LEN = 6
-CODE_TTL_SECONDS = 10 * 60
-MAX_ATTEMPTS = 6
-RESEND_COOLDOWN = 30
-EMAIL_RE = re.compile(r"^[^@\s]+@[^@\s]+\.[^@\s]+$")
-
-# ----------- Admin E-Mail(s) -----------
-ADMIN_CFG = st.secrets.get("admin_mail", {})
-ADMIN_EMAIL = ADMIN_CFG.get("admin_mail")
